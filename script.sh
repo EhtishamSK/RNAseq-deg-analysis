10f83715ef9b9e2fb921ab4d163f8a4c151bd25b
Ehtisham S. Khokhar <ehtishamshakeel@gmail.com>
2026-03-09 11:47:51 -0600

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,17 @@
+Step 1: Create Project Directories
+This project explores gene expression variation between two chile pepper cultivars (resistant and susceptible) using RNA-seq data collected from root samples 24 hours after inoculation.
+The analysis was performed between March and April 2024 using Linux and R for graduate coursework. 
+
+Create working directories
+mkdir final_project
+mkdir pepper_genome
+mkdir resistant_72
+mkdir susceptible_72
+
+These directories organize:
+| Directory      | Purpose                                |
+| -------------- | -------------------------------------- |
+| pepper_genome  | reference genome                       |
+| resistant_72   | RNA-seq reads for resistant cultivar   |
+| susceptible_72 | RNA-seq reads for susceptible cultivar |
+
